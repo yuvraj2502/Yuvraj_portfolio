5a539fd55adb67ee4b4a8c0d1e15401a9eadc580
yuvraj2502 <114469218+yuvraj2502@users.noreply.github.com>
2023-01-18 14:17:37 +0530

    updated contact view

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 from contact.models import Contact
-
+from django.core.mail import send_mail
+from . import settings
 
 
 def index(request):
@@ -17,9 +18,24 @@ def contact(request):
         
         contact = Contact(name=name,email=email,sub=sub,msg=msg)
         contact.save()
-        print(contact.name)
-    
+        
+        message=f'Hello {name}. Thank you for connecting with me. I will reply you soon on you message.'
+        subject="Mail from Yuvraj Verma"
+        send_mail_to(request,email,message,subject)
+        
+        message=f'Hello Yuvraj you got a mail from {email}. Sub = {sub} , message= {msg}.'
+        subject="Mail from Portfolio contact"
+        send_mail_to(request,"yuvrajv0504@gmail.com",message,subject)     
+        
     return render (request,'index.html')
 
+
+def send_mail_to(request,email,message,sub):
+    send_mail(sub,message,settings.EMAIL_HOST_USER,[email,])
+    
+    return render(request,'index.html')
+
+    
+    
 
         
